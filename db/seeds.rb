61dbf5cc1098ed4225495a5bb8e1eb53b6416fc5
NoritoshiKomaki <kmk.non.nrts@gmail.com>
2020-04-22 16:35:09 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |n|
+  User.create!(username: "ユーザー#{n + 1}", password: 111111)
+end
+
+10.times do |n|
+  Html.create!(score: 100 - n * 10 , game: 'html1', user_id: n + 1)
+end
+
+10.times do |n|
+  Css.create!(score: 100 - n * 10 , game: 'css1', user_id: n + 1)
+end
+
+10.times do |n|
+  Javascript.create!(score: 100 - n * 10 , game: 'js1', user_id: n + 1)
+end
