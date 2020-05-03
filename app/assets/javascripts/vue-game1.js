7705083cdc31404848706e60d96d8f16e07d794d
NoritoshiKomaki <kmk.non.nrts@gmail.com>
2020-05-03 18:43:37 +0900

    Update vue-game1.js

--- app/assets/javascripts/vue-game1.js
+++ app/assets/javascripts/vue-game1.js
@@ -40,6 +40,12 @@ $(function() {
     'lazy',
     'number',
     'trim',
+    'multiple',
+    '$event',
+    'directive',
+    'inserted',
+    'componentUpdated',
+    'unbind',
   ];
   
 var randoms = [];
