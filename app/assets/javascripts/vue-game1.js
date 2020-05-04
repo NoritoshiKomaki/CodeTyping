25d501ec2f84a3e97aec752f507df4154a7f92e1
NoritoshiKomaki <kmk.non.nrts@gmail.com>
2020-05-04 12:18:54 +0900

    Update vue-game1.js

--- app/assets/javascripts/vue-game1.js
+++ app/assets/javascripts/vue-game1.js
@@ -1,159 +1,148 @@
 $(function() {
   $('#length').append('/30');
   const words = [
-    'data','components','methods','watch','scoped',
-    'return','$mount','el','render','new Vue',
-    'props','computed','type','required','default',
-    '$emit','vm','v-bind','v-on','v-if',
-    'v-else-if','v-else','v-html','v-show','v-model',
-    'v-once','v-text','v-pre','v-cloak','slot',
-    'v-slot','template','is','keep-alive','activated',
-    'deactivated','lazy','number','trim','multiple',
-    '$event','directive','inserted','componentUpdated','unbind',
-    'binding',
-    'arg',
-    'modifiers',
-    'filter',
+    'data','components','methods','watch','scoped','return','$mount','el','render','new Vue','props','computed','required','default','$emit','vm','v-bind','v-on','v-if','v-else-if','v-else','v-html','v-show','v-model','v-once','v-text','v-pre','v-cloak','slot','v-slot','template','keep-alive','activated','deactivated','lazy','trim','multiple','$event','directive','inserted','componentUpdated','unbind','binding','arg','modifiers','filter','export','mixins','transition','transition-group','fade-enter',
+    'fade-leave',
   ];
   
-var randoms = [];
-var min = 0
-var max = words.length - 1;
+  var randoms = [];
+  var min = 0
+  var max = words.length - 1;
 
-for(i = min; i <= max; i++){
-  while(true){
-    var tmp = intRandom(min, max);
-    if(!randoms.includes(tmp)){
-      randoms.push(tmp);
-      console.log(tmp)
-      break;
+  for(i = min; i <= max; i++){
+    while(true){
+      var tmp = intRandom(min, max);
+      if(!randoms.includes(tmp)){
+        randoms.push(tmp);
+        console.log(tmp)
+        break;
+      }
     }
   }
-}
 
-function intRandom(min, max){
-  return Math.floor( Math.random() * (max - min + 1)) + min;
-}
+  function intRandom(min, max){
+    return Math.floor( Math.random() * (max - min + 1)) + min;
+  }
 
 
 // ここから固定
-const rank = $('#r-rank')
-const target = $('#target');
-const scoreLabel = $('#score');
-const missLabel = $('#miss');
-const timerLabel = $('#timer');
+  const rank = $('#r-rank')
+  const target = $('#target');
+  const scoreLabel = $('#score');
+  const missLabel = $('#miss');
+  const timerLabel = $('#timer');
 
-function countUp() {
-  const d = new Date(Date.now() - startTime + elapsedTime);
-  const m = String(d.getMinutes()).padStart(2, '0');
-  const s = String(d.getSeconds()).padStart(2, '0');
-  timerLabel.text(`${m}:${s}`);
+  function countUp() {
+    const d = new Date(Date.now() - startTime + elapsedTime);
+    const m = String(d.getMinutes()).padStart(2, '0');
+    const s = String(d.getSeconds()).padStart(2, '0');
+    timerLabel.text(`${m}:${s}`);
 
-  timeoutId = setTimeout(() => {
-    countUp();
-  }, 10);
-}
-
-function startTimer() {
-  testTimer=setInterval(function(){
-  second++;
-  } , 1000);
-}
-
-function countStop() {
-  clearTimeout(timeoutId);
-}
-
-function stopTimer(){
-  clearInterval(testTimer);
-}
-
-function updateTarget() {
-  let placeholder = '';
-  for (let i = 0; i < loc; i++) {
-    placeholder += '_';
+    timeoutId = setTimeout(() => {
+      countUp();
+    }, 10);
   }
-  target.text(placeholder + word.substring(loc));
-}
 
-function colorReset() {
-  $(document).keyup(function () {
-      target.css('color', '#333');
-  });
-}
-
-function showResult() {
-  const accuracy = score + miss === 0 ? 0 : score / (score + miss) * 100;
-  const totalScore =  (score - miss * 3) / (second / 60)
-  if (totalScore >= 300) {
-    rank.append('S');
-  } else if (totalScore >= 285) {
-    rank.append('A+');
-  } else if (totalScore >= 265) {
-    rank.append('A');
-  } else if (totalScore >= 250) {
-    rank.append('A-');
-  } else if (totalScore >= 235) {
-    rank.append('B+');
-  } else if (totalScore >= 215) {
-    rank.append('B');
-  } else if (totalScore >= 200) {
-    rank.append('B-');
-  } else if (totalScore >= 185) {
-    rank.append('C+'); 
-  } else if (totalScore >= 165) {
-    rank.append('C'); 
-  } else if (totalScore >= 150) {
-    rank.append('C-'); 
-  } else if (totalScore >= 135) {
-    rank.append('D+');
-  } else if (totalScore >= 115) {
-    rank.append('D');
-  } else if (totalScore >= 100) {
-    rank.append('D-');
-  } else if (totalScore >= 85) {
-    rank.append('E+');
-  } else if (totalScore >= 65) {
-    rank.append('E');
-  } else if (totalScore >= 50) {
-    rank.append('E-');
-  } else {
-    rank.append('-');
+  function startTimer() {
+    testTimer=setInterval(function(){
+    second++;
+    } , 1000);
   }
-  $('#r-totalScore').append(Math.round(totalScore));
-  $('#r-score').append(score);
-  $('#r-miss').append(miss);
-  $('#r-accuracy').append(`${accuracy.toFixed(2)}%`);
-  $('#r-timer').append(timerLabel);
-  $('#myscore').val(totalScore);
-}
 
-target.text(cnt).css('color', '#1da1f2').css('font-size', '64px')
-cnDown = setInterval(function(){ 
-    cnt--;
-    if(cnt <= 0){
-        clearInterval(cnDown);
+  function countStop() {
+    clearTimeout(timeoutId);
+  }
+
+  function stopTimer(){
+    clearInterval(testTimer);
+  }
+
+  function updateTarget() {
+    let placeholder = '';
+    for (let i = 0; i < loc; i++) {
+      placeholder += '_';
     }
-    target.text(cnt);
-},999);
+    target.text(placeholder + word.substring(loc));
+  }
 
-$(function(){
-  setTimeout(function(){
-    isPlaying = true;
-  
-    loc = 0;
-    score = 0;
-    miss = 0;
-    scoreLabel.text(score);
-    missLabel.text(miss);
-    word = words[randoms[0]];
-  
-    target.text(word).css('color', '#333').css('font-size', '48px')
-    startTime = Date.now();
-    countUp();
-    startTimer();
-  },3000);
-});
+  function colorReset() {
+    $(document).keyup(function () {
+        target.css('color', '#333');
+    });
+  }
+
+  function showResult() {
+    const accuracy = score + miss === 0 ? 0 : score / (score + miss) * 100;
+    const totalScore =  (score - miss * 3) / (second / 60)
+    if (totalScore >= 300) {
+      rank.append('S');
+    } else if (totalScore >= 285) {
+      rank.append('A+');
+    } else if (totalScore >= 265) {
+      rank.append('A');
+    } else if (totalScore >= 250) {
+      rank.append('A-');
+    } else if (totalScore >= 235) {
+      rank.append('B+');
+    } else if (totalScore >= 215) {
+      rank.append('B');
+    } else if (totalScore >= 200) {
+      rank.append('B-');
+    } else if (totalScore >= 185) {
+      rank.append('C+'); 
+    } else if (totalScore >= 165) {
+      rank.append('C'); 
+    } else if (totalScore >= 150) {
+      rank.append('C-'); 
+    } else if (totalScore >= 135) {
+      rank.append('D+');
+    } else if (totalScore >= 115) {
+      rank.append('D');
+    } else if (totalScore >= 100) {
+      rank.append('D-');
+    } else if (totalScore >= 85) {
+      rank.append('E+');
+    } else if (totalScore >= 65) {
+      rank.append('E');
+    } else if (totalScore >= 50) {
+      rank.append('E-');
+    } else {
+      rank.append('-');
+    }
+    $('#r-totalScore').append(Math.round(totalScore));
+    $('#r-score').append(score);
+    $('#r-miss').append(miss);
+    $('#r-accuracy').append(`${accuracy.toFixed(2)}%`);
+    $('#r-timer').append(timerLabel);
+    $('#myscore').val(totalScore);
+  }
+
+  target.text(cnt).css('color', '#1da1f2').css('font-size', '64px')
+  cnDown = setInterval(function(){ 
+      cnt--;
+      if(cnt <= 0){
+          clearInterval(cnDown);
+      }
+      target.text(cnt);
+  },999);
+
+  $(function(){
+    setTimeout(function(){
+      isPlaying = true;
+    
+      loc = 0;
+      score = 0;
+      miss = 0;
+      scoreLabel.text(score);
+      missLabel.text(miss);
+      word = words[randoms[0]];
+    
+      target.text(word).css('color', '#333').css('font-size', '48px')
+      startTime = Date.now();
+      countUp();
+      startTimer();
+    },3000);
+  });
 // ここまで
   
   window.addEventListener('keydown', e => {
